b7e73c44453301de6f45f49c1c5b8f2b26df7404
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-20 12:55:07 +0200

    Edit and Save functions are implemented

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  //   const [newName, setNewName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+  const editClickHandle = () => {
+    setIsEditing((prevState) => !prevState);
+  };
+  console.log(isEditing);
+  const inputHandle = (event) => {
+    name = event.target.value;
+  };
+  const onSaveHandle = () => {
+    console.log("save clicked");
+    setNewName(name);
+  };
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing === false ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <span>
+            <input
+              className="player-name"
+              onClick={inputHandle}
+              type="text"
+              name="playerName"
+            />
+          </span>
+        )}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      {isEditing === false ? (
+        <button onClick={editClickHandle}>Edit</button>
+      ) : (
+        <button onClick={onSaveHandle}>Save</button>
+      )}
     </li>
   );
 }
