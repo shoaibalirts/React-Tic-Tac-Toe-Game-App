c78993c08ea506dcfd1d66561d9814ede6d92bfc
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-22 13:35:14 +0200

    New hook for updating name to the input field is added

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
-  //   const [newName, setNewName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
-  const editClickHandle = () => {
-    setIsEditing((prevState) => !prevState);
+  const [newName, setNewName] = useState(name);
+  const handleEditClick = (event) => {
+    setIsEditing((prevState) => !prevState); // toggling between true and false
+    // if isEditing false then Edit, otherwise Save
+  };
+  const handleSaveClick = () => {
+    setIsEditing(false);
   };
   console.log(isEditing);
   const inputHandle = (event) => {
     name = event.target.value;
-  };
-  const onSaveHandle = () => {
-    console.log("save clicked");
     setNewName(name);
   };
 
@@ -19,25 +20,24 @@ export default function Player({ name, symbol }) {
     <li>
       <span className="player">
         {isEditing === false ? (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{newName}</span>
         ) : (
           <span>
             <input
-              className="player-name"
-              onClick={inputHandle}
+              onChange={inputHandle}
               type="text"
-              name="playerName"
+              required
+              value={newName}
             />
           </span>
         )}
 
         <span className="player-symbol">{symbol}</span>
       </span>
+      {isEditing && <button onClick={handleSaveClick}>Save</button>}
       {isEditing === false ? (
-        <button onClick={editClickHandle}>Edit</button>
-      ) : (
-        <button onClick={onSaveHandle}>Save</button>
-      )}
+        <button onClick={handleEditClick}>Edit</button>
+      ) : null}
     </li>
   );
 }
