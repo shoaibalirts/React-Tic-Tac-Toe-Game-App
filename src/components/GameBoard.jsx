b7796e1f687070e3d54221d110a5b7bebea26f47
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-22 19:14:56 +0200

    updated game board based on previous state

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 const initialGameBoard = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
 export default function GameBoard() {
-  const handleGetValue = (event) => {
-    console.log(event.target.value);
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  const handleSelectedSquare = (rowIndex, columnIndex) => {
+    // goal is to update the gameBoard dynamically
+    // 1) update only one Value, either X or O
+    //      a) we need to update the previous state so we have to use callback function
+    //      a) player 1 is using X and player 2 is using )
+    //      b) which player has clicked has to be founded out
+    //      c) place X or O on each grid item
+    //
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ]; // updated array by immutable way which is strongly recommended
+      updatedBoard[rowIndex][columnIndex] = "X"; // [0][0], [0][1], [0][2],
+      return updatedBoard;
+    });
   };
   return (
     <ol id="game-board">
-      {initialGameBoard.map((rowItem, rowIndex) => (
+      {gameBoard.map((rowItem, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {rowItem.map((playerSymbol, columnIndex) => (
               <li key={columnIndex}>
-                <button onClick={handleGetValue}>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSelectedSquare(rowIndex, columnIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
