436537fa3cfc52f5a1dafafd7ec10c628e647979
shoaibalirts <shoaibali.rts@gmail.com>
2024-07-23 14:01:41 +0200

    Combinations logic applied and gameBoard is updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/WinningCombinations.jsx";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
@@ -16,6 +24,31 @@ function App() {
   // then, pass it to Log for displaying grid value
   // const [activePlayer, setActivePlayer] = useState("X");
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard; // gameBoard is a derived state from gameTurns state managed in App component
+  // gameBoard is derived from turns state, which is managed in App.jsx
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player; // gameBoard[0][0]=X
+  }
+
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const ThirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === ThirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
   function handleSelectSquare(rowIndex, colIndex) {
     // handleSelectSquare is used for updating activePlayer state as well as updating gameTurns objects state
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
@@ -40,7 +73,8 @@ function App() {
           <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
           <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,38 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard; // gameBoard is a derived state from gameTurns state managed in App component
-  // gameBoard is derived from turns state, which is managed in App.jsx
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player; // gameBoard[0][0]=X
-  }
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-  //   const handleSelectedSquare = (rowIndex, columnIndex) => {
-  // goal is to update the gameBoard dynamically
-  // 1) update only one Value, either X or O
-  //      a) we need to update the previous state so we have to use callback function
-  //      a) player 1 is using X and player 2 is using )
-  //      b) which player has clicked has to be founded out
-  //      c) place X or O on each grid item
-  //
-  // setGameBoard((prevGameBoard) => {
-  //   const updatedBoard = [
-  //     ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //   ]; // updated array by immutable way which is strongly recommended
-  //   updatedBoard[rowIndex][columnIndex] = activePlayerSymbol; // [0][0], [0][1], [0][2],
-  //   return updatedBoard;
-  // });
-  // onSelectSquare();
-  //   };
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((rowItem, rowIndex) => (
+      {board.map((rowItem, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {rowItem.map((playerSymbol, columnIndex) => (
